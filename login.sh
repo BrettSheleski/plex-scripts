a8e5b8a8762c69e1dac9cc10bb3ed22c9133b3af
Brett Sheleski <brett@sheleski.com>
2016-10-05 13:57:05 -0500

    initial checkin

---
+++ login.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+credentials=`echo -n "$1":"$2"| base64`
+plexClientIdentifier="TESTSCRIPTV1"
+plexProduct="Test script"
+plexVersion="V1"
+
+url="https://plex.tv/users/sign_in.json"
+
+curl -s -X POST -H "Authorization: Basic $credentials" \
+             -H "X-Plex-Client-Identifier: $plexClientIdentifier"\
+             -H "X-Plex-Product: $plexProduct"\
+             -H "X-Plex-Version: $plexVersion"\
+             "$url"
+
