a3fcd95fac7b0b802c6bbb3087b5c056de97dbc9
Brett Sheleski <brett@sheleski.com>
2016-10-06 08:56:10 -0500

    removed plex-search.sh

--- plex-search.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-plexServer="localhost"
-plexPort=32400
-plexToken=`cat user.json | jq -r .user.authToken`
-
-searchTerm=$1
-
-curl -X GET  -H "X-Plex-Token: $plexToken"  http://"$plexServer":$plexPort/search?query=$searchTerm
